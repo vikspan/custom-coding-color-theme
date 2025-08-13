709f0b225865d41e519e773372a07fd8dae13a78
vikspan <viktorija.span@gmail.com>
2025-08-13 18:05:31 +0200

    add toggle button to CodeBlock

--- src/components/CodeBlock/CodeBlock.jsx
+++ src/components/CodeBlock/CodeBlock.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CodeBlock.scss";
+import Button from "../common/Button/Button";
 
-const CodeBlock = ({ colors }) => {
+const CodeBlock = ({ colors, hasButton }) => {
   const { typeSel, classSel, property, value } = colors;
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleButton = (
+    <Button
+      className="code-block__btn"
+      onClick={() => setIsDarkTheme(prev => !prev)}
+      variant="secondary"
+      size="sm"
+    >
+      {isDarkTheme ? "Light" : "Dark"}
+    </Button>
+  );
 
   return (
-    <div className="code-block">
+    <div className={"code-block" + (isDarkTheme ? " code-block--dark" : "")}>
       <small className="code-block__label">Lang: CSS</small>
+      {hasButton && toggleButton}
       <code className="code-block__code">
         <span style={{ color: typeSel }}>header</span> &#123; <br />
         &nbsp; <span style={{ color: property }}>display</span>: <span style={{ color: value }}>flex</span>; <br />
-        &nbsp; <span style={{ color: property }}>justify-content</span>: <span style={{ color: value }}>space-between</span>; <br />
+        &nbsp; <span style={{ color: property }}>justify-content</span>: <span style={{ color: value }}>center</span>; <br />
         &nbsp; <span style={{ color: property }}>align-items</span>: <span style={{ color: value }}>center</span>; <br />
         &#125; <br /><br />
-
         <span style={{ color: classSel }}>.logo</span> &#123; <br />
-        &nbsp; <span style={{ color: property }}>color</span>: <span style={{ color: value }}>#fff</span>; <br />
         &nbsp; <span style={{ color: property }}>font-size</span>: <span style={{ color: value }}>32px</span>; <br />
         &nbsp; <span style={{ color: property }}>font-weight</span>: <span style={{ color: value }}>300</span>; <br />
         &nbsp; <span style={{ color: property }}>text-transform</span>: <span style={{ color: value }}>uppercase</span>; <br />
